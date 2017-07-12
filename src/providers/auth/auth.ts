9d2706aa8b36b0d8bc343a81cf58c22083d6e4dc
yassine Badren <badrenyassine@gmail.com>
2017-07-13 01:34:27 +0200

    implémentation et test du service de authetificatin login password

---
+++ src/providers/auth/auth.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+/*
+  Generated class for the AuthProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class AuthProvider {
+
+  constructor() {
+
+  }
+
+}
