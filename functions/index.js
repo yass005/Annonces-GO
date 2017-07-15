5e9e220db424f6594a7d312822b662fdb265fdbd
yassine <badrenyassine@gmail.com>
2017-07-15 19:05:17 +0200

    cloud function for add user profile when register

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,17 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+exports.createProfile = functions.auth.user().onCreate( event => {
+ return admin.database().ref(`/userProfile/${event.data.uid}`).set({
+    firstName: event.data.firstName,
+    lastName: event.data.lastName,
+    birthDate: event.data.birthDate,
+    adress: {rue: "", numéro: "", ville: ""},
+    position: {Lat:"" , Long : ""},
+    email:  event.data.email
+
+  });
+});
